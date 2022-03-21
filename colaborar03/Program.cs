dea88ea9d3eb40846fd8fea1d62ead8968de5edd
Rafael Alberto Moreno Parra <ramsoftware@gmail.com>
2022-03-21 11:36:13 -0500

    Algoritmos genéticos. Programa 3.
    http://darwin.50webs.com/Espanol/Colaborar03.htm
    
    Para hacer pruebas con los algoritmos genéticos se requiere un conjunto de datos con las siguientes características:
    
    1. Tiene un patrón, es decir, una ecuación que los genera.
    
    2. Los datos deben estar normalizados. Tanto X como Y deben estar entre 0 y 1.
    
    3. El valor de Y debe ser distinto dependiendo de X. El gráfico NO debe ser una línea horizontal.
    
    4. El valor de Y debe tener subidas y bajadas. El gráfico NO debe ser una simple diagonal.

---
+++ colaborar03/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace GeneraDataset {
+    class MainClass {
+        public static void Main(string[] args) {
+            Random Azar = new Random();
+            GeneradorDatos genera = new GeneradorDatos();
+            genera.GeneraEcuacion(Azar, 5, 100);
+        }
+    }
+}
