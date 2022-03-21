e252f71355efbb149059336e478d7dacc4a8cb35
Rafael Alberto Moreno Parra <ramsoftware@gmail.com>
2022-03-21 11:40:49 -0500

    Algoritmos genéticos. Programa 5. Genético vs Neuronal
    http://darwin.50webs.com/Espanol/Colaborar05.htm
    
    Comparando el algoritmo genético contra una red neuronal (perceptrón multicapa).
    
    En la comparación anterior, lo hizo mejor el algoritmo genético que mejora primero los individuos. En esta ocasión, se compara ese algoritmo genético contra una red neuronal (perceptrón multicapa).
    
    1. Se genera al azar una ecuación.
    
    2. Esta ecuación genera un conjunto de datos (dataset) de tipo X,Y.
    
    3. Se reescribieron los algoritmos para que usaran la misma cantidad de tiempo y así comparar cuál es más eficiente.
    
    4. Tanto el algoritmo genético como la red neuronal intentarán cubrir todo el conjunto de datos con una sola ecuación.
    
    5. Se repite 50 veces desde el punto 1. En cada repetición se varía el tamaño de la población.

---
+++ colaborar05/GeneradorDatos.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AGMvsRN {
+    public class GeneradorDatos {
+        //Almacena los valores de la variable independiente (entrada)
+        public List<double> Entradas = new List<double>();
+
+        //Almacena los valores de la variable dependiente (salida) 
+        public List<double> Salidas = new List<double>();
+
+        //Ecuación generada al azar
+        List<Pieza> Piezas = new List<Pieza>();
+
+        //Usada para detectar si la ecuación generada al azar NO genera errores matemáticos
+        bool HayError;
+
+        //Genera la ecuación 
+        public void GeneraEcuacion(Random Azar, int numPiezas, double numDatosDataSet) {
+
+            //Si el mínimo valor y el máximo valor son muy parecidos, entonces la ecuación está generando una
+            //línea horizontal de datos, sin cambios, eso no es aceptable.
+            double MinimoValor, MaximoValor;
+
+            //Evita que la ecuación genere simples líneas diagonales
+            int Ysube, Ybaja;
+            double Yanterior;
+
+            do {
+                HayError = false;
+                MinimoValor = double.MaxValue;
+                MaximoValor = double.MinValue;
+                Ysube = 0;
+                Ybaja = 0;
+                Yanterior = 0;
+
+                //Genera las piezas al azar que componen la ecuación
+                Piezas.Clear();
+                for (int cont = 0; cont < numPiezas; cont++) Piezas.Add(new Pieza(Azar, cont));
+
+                //Evalúa con los datos de entrada de x de 0 a 1
+                double avance = 1 / numDatosDataSet;
+                for (double x = 0; x <= 1; x += avance) {
+                    double y = Evaluar(x);
+                    if (HayError) break;
+
+                    //Si el mínimo valor y el máximo valor son muy parecidos, entonces la ecuación está generando una
+                    //línea horizontal de datos, sin cambios, eso no es aceptable.
+                    if (y < MinimoValor) MinimoValor = y;
+                    if (y > MaximoValor) MaximoValor = y;
+
+                    //Evita que la ecuación genere simples líneas diagonales
+                    if (x > 0 && y > Yanterior) Ysube++;
+                    if (x > 0 && y < Yanterior) Ybaja++;
+                    Yanterior = y;
+                }
+
+                if (MaximoValor - MinimoValor <= 0.05 || Ysube == 0 || Ybaja == 0) HayError = true;
+                if (HayError) continue;
+
+                //Si no hay errores, entonces normaliza los valores y los guarda.
+                Entradas.Clear();
+                Salidas.Clear();
+                for (double x = 0; x <= 1; x += avance) {
+                    double y = (Evaluar(x) - MinimoValor) / (MaximoValor - MinimoValor);
+                    Entradas.Add(x);
+                    Salidas.Add(y);
+                }
+
+
+                //Si la ecuación genera errores o genera una línea horizontal de datos o es una simple diagonal entonces se genera otra 
+            } while (HayError);
+        }
+
+        //Se va de pieza en pieza evaluando toda la ecuación
+        public double Evaluar(double x) {
+            double resultado = 0;
+
+            for (int pos = 0; pos < Piezas.Count; pos++) {
+                Pieza tmpPieza = Piezas[pos];
+                double numA, numB;
+
+                switch (tmpPieza.TipoA) {
+                    case 0: numA = tmpPieza.NumeroA; break;
+                    case 1: numA = x; break;
+                    default: numA = Piezas[tmpPieza.PiezaA].ValorPieza; break;
+                }
+
+                switch (tmpPieza.TipoB) {
+                    case 0: numB = tmpPieza.NumeroB; break;
+                    case 1: numB = x; break;
+                    default: numB = Piezas[tmpPieza.PiezaB].ValorPieza; break;
+                }
+
+                switch (tmpPieza.Operador) {
+                    case 0: resultado = numA * numB; break;
+                    case 1: resultado = numA / numB; break;
+                    case 2: resultado = numA + numB; break;
+                    case 3: resultado = numA - numB; break;
+                    default: resultado = Math.Pow(numA, numB); break;
+                }
+
+                if (double.IsNaN(resultado) || double.IsInfinity(resultado)) {
+                    HayError = true;
+                    return 0;
+                }
+
+                switch (tmpPieza.Funcion) {
+                    case 0: resultado = Math.Sin(resultado); break;
+                    case 1: resultado = Math.Cos(resultado); break;
+                    case 2: resultado = Math.Tan(resultado); break;
+                    case 3: resultado = Math.Abs(resultado); break;
+                    case 4: resultado = Math.Asin(resultado); break;
+                    case 5: resultado = Math.Acos(resultado); break;
+                    case 6: resultado = Math.Atan(resultado); break;
+                    case 7: resultado = Math.Log(resultado); break;
+                    case 8: resultado = Math.Ceiling(resultado); break;
+                    case 9: resultado = Math.Exp(resultado); break;
+                    case 10: resultado = Math.Sqrt(resultado); break;
+                    case 11: resultado = Math.Pow(resultado, 0.3333333333333333333333); break;
+                }
+
+                if (double.IsNaN(resultado) || double.IsInfinity(resultado)) {
+                    HayError = true;
+                    return 0;
+                }
+
+                tmpPieza.ValorPieza = resultado;
+            }
+            return resultado;
+        }
+    }
+}
---
+++ colaborar05/Program.cs
@@ -0,0 +1,129 @@
+﻿/* Autor: Rafael Alberto Moreno Parra
+Fecha: 15 de marzo de 2022
+Algoritmo evolutivo Mejorado vs Red Neuronal
+
+*/
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Management;
+
+namespace AGMvsRN {
+    internal class Program {
+        static void Main(string[] args) {
+            Random Azar = new Random(); //Generador único
+
+            int numPruebas = 50; //Número de veces que se generará un dataset con ecuación aleatoria para probar ambos algoritmos
+            int numDatosDataSet = 100; //Cuántos registros tendrá cada dataset
+            long TiempoParaOperar = 20000; //Cuántos milisegundos dará a cada tipo de algoritmo genético para operar
+			int numPiezas = 5; //Número de piezas que compondrá la ecuación que genera el dataset
+
+			//Configuración del algoritmo genético
+			int IndividuosPorPoblacion = 100;
+			int RangoCoeficientes = 20; //Rango para crear los coeficientes de los individuos.
+
+			//Configuración del perceptrón multicapa
+			int capa0 = 5; //Total neuronas en la capa 0
+			int capa1 = 5; //Total neuronas en la capa 1
+
+			Console.WriteLine("Fecha: " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+			Console.WriteLine("Total Pruebas: " + numPruebas.ToString());
+			Console.WriteLine("Número de registros por conjunto de datos: " + numDatosDataSet.ToString());
+			Console.WriteLine("Tiempo en milisegundos para cada algoritmo: " + TiempoParaOperar.ToString());
+			Console.WriteLine("Número de piezas que compondrá la ecuación que genera el dataset: " + numPiezas.ToString());
+			Console.WriteLine("Velocidad PC: " + GetCpuSpeed().ToString());
+
+			Console.WriteLine("\r\n\r\nALGORITMO GENETICO: MEJORA LOS INDIVIDUOS Y LUEGO COMPITEN");
+			Console.WriteLine("Número de individuos en la población: " + IndividuosPorPoblacion.ToString());
+			Console.WriteLine("Rango de Coeficientes: " + RangoCoeficientes.ToString());
+
+			Console.WriteLine("\r\n\r\nPERCEPTRON MULTICAPA");
+			Console.WriteLine("Número de neuronas Capa 0: " + capa0.ToString());
+			Console.WriteLine("Número de neuronas Capa 1: " + capa1.ToString() + "\r\n");
+
+			//Prepara el generador de datos
+			GeneradorDatos genera = new GeneradorDatos();
+
+            for (int pruebas = 1; pruebas <= numPruebas; pruebas++) {
+
+                //Genera el dataset en forma aleatoria
+                genera.GeneraEcuacion(Azar, numPiezas, numDatosDataSet);
+
+                //Llama al algoritmo genético que mejora primero los individuos
+                GeneticoB(Azar, genera.Entradas, genera.Salidas, 0, genera.Entradas.Count, TiempoParaOperar, IndividuosPorPoblacion, RangoCoeficientes);
+
+                //Llama a la red neuronal
+                Perceptron(Azar, genera.Entradas, genera.Salidas, 0, genera.Entradas.Count, TiempoParaOperar, capa0, capa1);
+            }
+
+            Console.WriteLine("\r\nFINAL\r\n");
+            //Console.ReadKey();
+        }
+
+        public static void GeneticoB(Random Azar, List<double> Entradas, List<double> Salidas, int posInicial, int posFinal, long TiempoParaOperar, int IndividuosPorPoblacion, int RangoCoeficientes) {
+
+            PoblacionB poblacionB = new PoblacionB(Azar, IndividuosPorPoblacion, RangoCoeficientes);
+            poblacionB.Proceso(Azar, Entradas, Salidas, posInicial, posFinal, TiempoParaOperar);
+
+            Console.Write("AGM;" + poblacionB.MejorAproximacion().ToString());
+        }
+
+        //La red neuronal
+        public static void Perceptron(Random Azar, List<double> Entradas, List<double> Salidas, int posInicial, int posFinal, long TiempoParaOperar, int capa0, int capa1) {
+			//Medidor de tiempos
+			Stopwatch cronometro = new Stopwatch();
+			cronometro.Reset();
+			cronometro.Start();
+
+			int numEntradas = 1; //Número de entradas
+			int capa2 = 1; //Total neuronas en la capa 2
+			Perceptron perceptron = new Perceptron(numEntradas, capa0, capa1, capa2);
+
+			//Estas serán las entradas externas al perceptrón
+			List<double> entradas = new List<double>();
+			entradas.Add(0);
+
+			//Estas serán las salidas esperadas externas al perceptrón
+			List<double> salidaEsperada = new List<double>();
+			salidaEsperada.Add(0);
+
+			//Ciclo que entrena la red neuronal
+			while (cronometro.ElapsedMilliseconds < TiempoParaOperar) {
+
+				//Por cada ciclo, se entrena el perceptrón con todos los valores
+				for (int conjunto = 0; conjunto < Entradas.Count; conjunto++) {
+					//Entradas y salidas esperadas
+					entradas[0] = Entradas[conjunto];
+					salidaEsperada[0] = Salidas[conjunto];
+
+					//Primero calcula la salida del perceptrón con esas entradas
+					perceptron.calculaSalida(entradas);
+
+					//Luego entrena el perceptrón para ajustar los pesos y umbrales
+					perceptron.Entrena(entradas, salidaEsperada);
+				}
+			}
+
+			//Console.WriteLine("Entrada normalizada | Salida esperada normalizada | Salida perceptrón normalizada");
+			double Aproxima = 0;
+			for (int conjunto = 0; conjunto < Entradas.Count; conjunto++) {
+				//Entradas y salidas esperadas
+				entradas[0] = Entradas[conjunto];
+				salidaEsperada[0] = Salidas[conjunto];
+
+				//Calcula la salida del perceptrón con esas entradas
+				perceptron.calculaSalida(entradas);
+				Aproxima += (perceptron.capas[2].salidas[0] - salidaEsperada[0]) * (perceptron.capas[2].salidas[0] - salidaEsperada[0]);
+			}
+
+			Console.WriteLine(";RN;" + Aproxima.ToString());
+		}
+
+		static public uint GetCpuSpeed() {
+			var managementObject = new ManagementObject("Win32_Processor.DeviceID='CPU0'");
+			var speed = (uint)(managementObject["CurrentClockSpeed"]);
+			managementObject.Dispose();
+			return speed;
+		}
+	}
+}
